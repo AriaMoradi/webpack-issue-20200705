9ee08ec43e953003332375e52a9189024ec4ab06
Aria Moradi <aria.moradi007@gmail.com>
2020-07-03 21:13:52 +0430

    show images

--- course/models.py
+++ course/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.templatetags.static import StaticNode
 
 
 class Course(models.Model):
     title = models.CharField(max_length=256)
     slug = models.CharField(max_length=256, unique=True)
-    image = models.ImageField()
+    image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.title
